a204efc291dcbcac020838634443ce366cd34311
Nicolás Guzmán <nguzmanespinoza1@gmail.com>
2024-03-21 15:50:48 -0300

    proyecto casi terminado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Valores de la Carta
 let arrayNumeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
 let arrayPintas = ["♦", "♥", "♠", "♣"];
+let arrayValores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 // Div donde van las cartas Generadas
 let divCartasTop = document.querySelector('.cartasRandomDiv');
 // Definiendo Variables de la Carta
@@ -13,7 +14,9 @@ let arrayDeCartas = [];
 function crearCarta() {
     // Creando Parametros Random
     let pintaRandom = arrayPintas[Math.floor(Math.random() * 4)];
-    let numeroRandom = arrayNumeros[Math.floor(Math.random() * 13)];
+    let valorRandom = Math.floor(Math.random() * 13)
+    let numeroRandom = arrayNumeros[valorRandom];
+    let valorCarta = arrayValores[valorRandom]
     // Creando elementos HTML
     let cartaAleatoria = document.createElement('div');
     let pintaTopRandom = document.createElement('p');
@@ -47,7 +50,7 @@ function crearCarta() {
     divCartasTop.appendChild(cartaAleatoria);
 
     // Agregando Cartas al Array 
-    arrayDeCartas.push({numero:numeroRandom, pinta:pintaRandom});
+    arrayDeCartas.push({numero:numeroRandom, pinta:pintaRandom, valor:valorCarta});
 
 }
 // Obteniendo Valor del Input
@@ -57,7 +60,7 @@ inputDraw.addEventListener('click', tenCards);
 
 function tenCards() {
     divCartasTop.innerHTML = "";
-    inputCarta = document.querySelector('#inputCarta').value;
+    inputCarta = parseInt(document.querySelector('#inputCarta').value);
     arrayDeCartas = [];
     for(let i = 1; i <= inputCarta ; i++){
     crearCarta()};
@@ -65,13 +68,16 @@ function tenCards() {
     
 }
 
-// Obteniend el boton Sort
+// Obteniendo el boton Sort
 
 inputSort = document.querySelector('.sort');
-inputSort.addEventListener('click', ordenar(arrayDeCartas));
+inputSort.addEventListener('click', function() {
+    sortedBoard.innerHTML = ""
+    ordenar(arrayDeCartas)
+});
 
 // Obteniendo Div donde van las cartas ordenadas
-let cartasOrdenadas = document.querySelector('.cartasOrdenadas')
+let sortedBoard = document.querySelector('.sortedCardBoard')
 
 // Función para ordenar Cartas
 
@@ -79,16 +85,63 @@ const ordenar = (arr) => {
     let min = 0;
     while (min < arr.length-1){
         for(let i = min+1; i < arr.length; i++) {
-          if (arr[min].numero > arr[i].numero) {
+          if (arr[min].valor > arr[i].valor) {
             let aux = arr[min];
             arr[min] = arr[i];
             arr[i] = aux;
           }
         }   
-        min++;
-        console.log("Ciclo while terminado")
+    // Creando Divs para las Cartas Div Row y Div Col
+        let divParaCartasRow = document.createElement('div')
+        let divParaCartasCol = document.createElement('div')
 
+    // Agregando Clases a los Div
+        divParaCartasRow.classList.add('row')
+        divParaCartasCol.classList.add('col-md-12', 'd-flex', 'mt-5', 'mb-3')
+
+    // Agregando Childs del div sortedCardBoard
+        sortedBoard.appendChild(divParaCartasRow)
+        divParaCartasRow.appendChild(divParaCartasCol)
+
+
+        // Agregando Cartas al div cartasOrdenadas, al final del ciclo while
+        for(let i = 0; i < arr.length;i++) {
+            let cartaAleatoria = document.createElement('div');
+            let pintaTopRandom = document.createElement('p');
+            let numeroCenter = document.createElement('p');
+            let pintaBotRandom = document.createElement('p');
+            
+        
+            // Agregando Clases
+            cartaAleatoria.classList.add('carta');
+            pintaTopRandom.classList.add('pintaTop');
+            numeroCenter.classList.add('numero');
+            pintaBotRandom.classList.add('pintaBot');
+            // Agregando Pinta
+            pintaTopRandom.textContent = arr[i].pinta;
+            pintaBotRandom.textContent = arr[i].pinta;
+            // Agregando Numero Random
+            numeroCenter.textContent = arr[i].numero;
+            // Agregando Estilos 
+            if(arr[i].pinta == arrayPintas[0] || arr[i].pinta == arrayPintas[1]) {
+                pintaTopRandom.style.color = 'red';
+                pintaBotRandom.style.color = 'red';
+                numeroCenter.style.color = 'red';
+            } else {
+                pintaTopRandom.style.color = 'black';
+                pintaBotRandom.style.color = 'black';
+                numeroCenter.style.color = 'black';
+            }
+            // Agregando Child
+            
+            cartaAleatoria.appendChild(pintaTopRandom);
+            cartaAleatoria.appendChild(numeroCenter);
+            cartaAleatoria.appendChild(pintaBotRandom);
+            divParaCartasCol.appendChild(cartaAleatoria);
+            
+        
+            console.log("Ciclo while terminado") }
+    min++
     }
-	return arr;
+	;
 }; 
-
